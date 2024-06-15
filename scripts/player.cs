23fa35945ff02950920d76b9f56f688fc113f643
Valionos <cellttc@live.com>
2024-06-15 15:47:25 -0400

    bird fly corectly

--- scripts/player.cs
+++ scripts/player.cs
@@ -6,70 +6,58 @@ using System;
 
 public partial class player : CharacterBody2D
 {
-	Vector2 accel = Vector2.Zero;
-	Vector2 vel = Vector2.Zero;
 	
-	float gravity = 5;
-	float speed = 10;
-	float friction = 1;
+	//Vector2 vel = Vector2.Zero; in memorandium
+	Vector2 initialPosition;
 	
+	float gravity = 5000;
+	float accel = 10000;
+	float friction = .1F;
+
 	
 	
 	public override void _Ready()
 	{
-		
+		Vector2 initialPosition = Position;
 	}
 		
 
 	public override void _Process(double delta)
 	{
 		
-		
+		Vector2 vel = Velocity;
 		
 		var animated = GetNode<AnimatedSprite2D>("PelicanBody");
 		
 		
-		if(GetViewport().GetMousePosition().Y < Position.Y)
+		if(Input.IsActionPressed("move_up"))
 		{
-			 accel.Y -= speed;
-			 animated.Play("gain");
+			vel.Y -= accel * (float) delta; 
+			animated.Play("gain");
+
 			
 		}else
 		{
 			animated.Play("fall");
 		}
 		
-		
-		if(Input.IsActionPressed("move_down"))
+		if(!IsOnFloor()) 
 		{
-			 accel.Y += speed;
+			vel.Y += gravity * (float) delta;
 		}
+
 		
-		
-		if(!IsOnFloor())
-		{
-		accel.Y += gravity;
-		vel += accel;
-		}else
-		{
-			accel.Y = 0;
-			vel.Y = 0;
-		}
-		
-		Mathf.MoveToward(accel.Y, 0, friction);
-		
-		
-		if(Input.IsActionPressed("debug_stop"))
-		{
-			vel = Vector2.Zero;
-		}
-		
-		
-		Position += vel * (float)delta;
+		vel.Y = Mathf.MoveToward(vel.Y, 0, Math.Abs(vel.Y) * friction); //TODO: make friction curve and have alot of fun of form y=x^friction
+	
+		Velocity = vel;
+		MoveAndSlide();
 		
 
 		
 	}
+
+
+	
 
 }
 
