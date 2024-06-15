57ba6eb5632cbf3f35ae12321e738fd262b05eda
Valionos <cellttc@live.com>
2024-06-15 17:42:28 -0400

    bad rotations

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,19 +1,21 @@
 using Godot;
 using System;
+using System.Runtime.ConstrainedExecution;
 
 
 
 
 public partial class player : CharacterBody2D
 {
-	
+	//max speed 750
 	//Vector2 vel = Vector2.Zero; in memorandium
 	Vector2 initialPosition;
 	
-	float gravity = 5000;
+	static readonly float upAngle = .5236F;
+	static readonly float downAngle = 1.57F; 
+	float gravity = 2500;
 	float accel = 10000;
 	float friction = .1F;
-
 	
 	
 	public override void _Ready()
@@ -24,18 +26,16 @@ public partial class player : CharacterBody2D
 
 	public override void _Process(double delta)
 	{
-		
+		float maxSpeed = 750;//Mathf.MoveToward(Math.Abs(accel - gravity), 0, Math.Abs(accel - gravity) * friction);
+
 		Vector2 vel = Velocity;
 		
-		var animated = GetNode<AnimatedSprite2D>("PelicanBody");
-		
+		var animated = GetNode<AnimatedSprite2D>("%PelicanBody");
 		
 		if(Input.IsActionPressed("move_up"))
 		{
 			vel.Y -= accel * (float) delta; 
 			animated.Play("gain");
-
-			
 		}else
 		{
 			animated.Play("fall");
@@ -48,7 +48,19 @@ public partial class player : CharacterBody2D
 
 		
 		vel.Y = Mathf.MoveToward(vel.Y, 0, Math.Abs(vel.Y) * friction); //TODO: make friction curve and have alot of fun of form y=x^friction
-	
+
+		//rotation
+		float rotateAmt = vel.Y/maxSpeed;
+		if(rotateAmt >= 0)
+		{
+			GetNode<Marker2D>("%PelicanCenter").Rotation = rotateAmt * downAngle;
+		} else
+		{
+			GetNode<Marker2D>("%PelicanCenter").Rotation = rotateAmt * upAngle;
+		}
+		
+		GD.Print(vel.Y);
+
 		Velocity = vel;
 		MoveAndSlide();
 		
